d7b1d247b75422dbdc3756aa782318e7686af67d
andrea.debortoli <andrea.debortoli@AULAS51-PC21.itib.local>
2023-10-13 12:09:09 +0200

    inizio condominio

--- src/main/java/amministrazioneCondomini/Appartamento.java
+++ src/main/java/amministrazioneCondomini/Appartamento.java
@@ -20,5 +20,35 @@ public class Appartamento {
         this.millesimi = a.millesimi;
         this.numeroApp = a.numeroApp;
     }
+
+    public String getNominativoProprietario() {
+        return nominativoProprietario;
+    }
+    public double getMillesimi() {
+        return millesimi;
+    }
+    public int getNumeroApp() {
+        return numeroApp;
+    }
+    public void setNominativoProprietario(String nominativoProprietario) {
+        this.nominativoProprietario = nominativoProprietario;
+    }
+    public void setMillesimi(double millesimi) {
+        this.millesimi = millesimi;
+    }
+    public void setNumeroApp(int numeroApp) {
+        this.numeroApp = numeroApp;
+    }
+
+    @Override
+    public String toString() {
+        return "Appartamento{"
+                + "\n\tnominativoProprietario: " + nominativoProprietario
+                + "\n\tmillesimi: " + millesimi 
+                + "\n\tnumeroAppartamento: " + numeroApp
+                + "\n}"; 
+    }
+    
+    
     
 }
--- src/main/java/amministrazioneCondomini/Condomini.java
+++ src/main/java/amministrazioneCondomini/Condomini.java
@@ -4,6 +4,49 @@ package amministrazioneCondomini;
  *
  * @author andrea.debortoli
  */
-public class Condomini {
-    
+public class Condominio {
+   
+    private String nome;
+    private String indirizzo;
+    private Appartamento[] appartamenti;
+   
+    public Condominio(String nome, String indirizzo,Appartamento[] app) {
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        appartamenti = copia(app);
+    }
+   
+    public Condominio(Condominio c){
+        this.nome = c.nome;
+        this.indirizzo = c.indirizzo;
+        this.appartamenti = copia(c.appartamenti);
+    }
+   
+    public int numeroAppartamenti() {
+        return appartamenti.length;
+    }
+   
+    public String nominativoMillesimiMax() {
+        return appartamenti[trovaMaxMillesimi()].getNominativoProprietario();
+    }
+   
+   
+    private int trovaMaxMillesimi() {
+        int pos = 0;
+        for (int i = 1; i < appartamenti.length; i++){
+            if (appartamenti[pos].getMillesimi() < appartamenti[i].getMillesimi())
+                pos = i;
+        }
+        return pos;
+    }
+   
+   
+    private Appartamento[] copia(Appartamento[] app){
+        Appartamento[] temp = new Appartamento[app.length];
+        for (int i = 0; i < app.length; i++){
+            temp[i] = app[i];
+        }
+        return temp;
+    }
+   
 }
