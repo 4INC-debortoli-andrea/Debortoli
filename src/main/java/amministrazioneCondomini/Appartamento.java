af6ba0402794d7bb94bb5b1f35b703bc408a08cf
andrea.debortoli <andrea.debortoli@AULAS51-PC21.itib.local>
2023-10-13 11:32:29 +0200

    inizio di Appartamento

--- src/main/java/amministrazioneCondomini/Appartamento.java
+++ src/main/java/amministrazioneCondomini/Appartamento.java
@@ -5,5 +5,20 @@ package amministrazioneCondomini;
  * @author andrea.debortoli
  */
 public class Appartamento {
+    private String nominativoProprietario;
+    private double millesimi;
+    private int numeroApp;
+    
+    public Appartamento(String nominativo, double millesimi, int numeroApp){
+        this.nominativoProprietario = nominativo;
+        this.millesimi = millesimi;
+        this.numeroApp = numeroApp;
+    }
+    
+    public Appartamento (Appartamento a){
+        this.nominativoProprietario = a.nominativoProprietario;
+        this.millesimi = a.millesimi;
+        this.numeroApp = a.numeroApp;
+    }
     
 }
