7566daea0b91dc7fc950ebc2c94a655cf556bc8a
4INC-debortoli-andrea <andrea.debortoli@buonarroti.tn.it>
2023-11-02 22:49:53 +0100

    Aggiunto classe

--- src/main/java/classe/Classe.java
+++ src/main/java/classe/Classe.java
@@ -5,5 +5,13 @@ package classe;
  * @author andrea.debortoli
  */
 public class Classe {
+    public Studente capoClasse;
+    public Studente[] studenti;
     
+    public Classe(Studente capo, Studente[] studenti){
+        capoClasse = capo;
+        for(int i = 0; i < studenti.length; i++){
+            
+        }
+    }
 }
--- src/main/java/classe/Studente.java
+++ src/main/java/classe/Studente.java
@@ -5,5 +5,38 @@ package classe;
  * @author andrea.debortoli
  */
 public class Studente {
+    private String cognome;
+    private String nome;
+
+    public Studente(String cognome, String nome) {
+        this.cognome = cognome;
+        this.nome = nome;
+    }
     
+    public Studente(Studente studente) {
+        this.cognome = studente.getCognome();
+        this.nome = studente.getNome();        
+    }
+    
+    public String getCognome() {
+        return cognome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setCognome(String c){
+        nome = c;
+    }
+    public void setNome(String n){
+        cognome = n;
+    }
+    
+    @Override
+    public String toString(){
+    return "Studente{ " + "\n" +
+            "\tcognome: " + cognome + "\n" +
+            "\tnome: " + nome + "\n" +
+            "}"; 
+    }
 }
