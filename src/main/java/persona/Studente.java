badd28065c60694b8ddbcbfbc3c66db775d6d8db
andrea.debortoli <andrea.debortoli@buonarroti.tn.it>
2024-01-19 12:12:28 +0100

    lezione 19/01

--- src/main/java/persona/Studente.java
+++ src/main/java/persona/Studente.java
@@ -5,13 +5,20 @@ package persona;
  * @author andry
  */
 
+import java.util.ArrayList;
+import java.util.Comparator; 
 import data.Data;
+enum TipoOrdinamento{
+    CRESCENTE,
+    DECRESCENTE
+}
+
 public class Studente extends Persona {
 
     private final String SCUOLA = "I.T.T. Buonarroti";
     private Integer classe;
     private Boolean isRipetente;
-    private Float[] voti;
+    private ArrayList<Float> voti;
 
     public Studente() {
         super();
@@ -28,7 +35,62 @@ public class Studente extends Persona {
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
-        this.voti = new Float[0];
+        this.voti = new ArrayList<Float>();
+    }
+
+    
+    public final void setClasse(Integer classe) throws Exception {
+        if (classe == null) {
+            throw new Exception("Il parametro non può essere null");
+        } else if (classe < 1 || classe > 5) {
+            throw new Exception("La classe deve essere compresa tra 1 e 5");
+        } else {
+            this.classe = classe;
+        }
+    }
+    
+    public final void setIsRipetente(Boolean isRipetente) throws Exception {
+        if (classe == null) {
+            throw new Exception("Il parametro non può essere null");
+        } else {
+            this.isRipetente = isRipetente;
+        }
+    }
+    
+    public final void setVoti(Float[] voti) throws Exception {
+        if (voti == null) 
+        throw new Exception("Il parametro inserito non può essere null");
+        else {
+            for (int i = 0; i < voti.length; i++) {
+                aggiungiVoto(voti[i]);                
+            }
+        }
+    }
+    
+    public final String getSCUOLA() {
+        return SCUOLA;
+    }
+    
+    public final Boolean getIsRipetente() {
+        return isRipetente;
+    }
+    
+    public final Integer getClasse() {
+        return classe;
+    }
+    
+    public void aggiungiVoto(Float voto) throws Exception {
+        if (voto == null) {
+            throw new Exception("Il voto deve essere compreso tra 0 e 10 e non può essere null");
+        }
+        if (voto.floatValue() < 3 || voto.floatValue() > 10) {
+            throw new Exception("Il voto deve essere compreso tra 0 e 10");
+        }
+        if (voti == null) {
+            throw new Exception("L'array non può essere null");
+        } else {  
+            voti.add(voto);
+        }
     }
 
     public Boolean verificaOmonimia(Studente studente) throws Exception {
@@ -49,82 +111,88 @@ public class Studente extends Persona {
         }
 
     }
-
-    public final void setClasse(Integer classe) throws Exception {
-        if (classe == null) {
-            throw new Exception("Il parametro non può essere null");
-        } else if (classe < 1 || classe > 5) {
-            throw new Exception("La classe deve essere compresa tra 1 e 5");
+    
+    public void rimuoviUltimoVoto()throws Exception {
+        if(voti == null){
+            throw new Exception("Non può essere null");
         } else {
-            this.classe = classe;
-        }
-    }
-
-    public final void setIsRipetente(Boolean isRipetente) throws Exception {
-        if (classe == null) {
-            throw new Exception("Il parametro non può essere null");
-        } else {
-            this.isRipetente = isRipetente;
-        }
-    }
-
-    public final void setVoti(Float[] voti) throws Exception {
-        if (voti == null) 
-            throw new Exception("Il parametro inserito non può essere null");
-        else {
-            for (int i = 0; i < voti.length; i++) {
-                aggiungiVoti(voti[i]);                
+            if(voti.size() == 0){
+                throw new Exception("Non puoi rimuovere più voti");
+            } else{
+                voti.removeLast();
             }
-        }
-    }
-
-
-
-
-    public final String getSCUOLA() {
-        return SCUOLA;
-    }
-
-    public final Boolean getIsRipetente() {
-        return isRipetente;
-    }
-
-    public final Integer getClasse() {
-        return classe;
-    }
-
-    public void aggiungiVoti(Float voto) throws Exception {
-        if (voto == null) {
-            throw new Exception("Il voto deve essere compreso tra 0 e 10 e non può essere null");
-        }
-        if (voto < 0 || voto > 10) {
-            throw new Exception("Il voto deve essere compreso tra 0 e 10");
-        }
-        if (voti == null) {
-            throw new Exception("L'array non può essere null");
-        } else {
-            Float[] temp = new Float[voti.length+1];
-            for (int i = 0; i < voti.length; i++){
-                temp[i] = voti[i];
-            }
-            temp[voti.length] = voto;
-            voti = temp;
-        }
+        } 
     }
 
     public void rimuoviVoto(Integer posizione) throws Exception {
-        if (posizione >= voti.length || posizione < 0) {
-            throw  new Exception("La posizione supera i limiti dell'array");
+        if(voti == null){ 
+            throw new Exception("Non può essere null");
         }
-        Float[] temp = new Float[voti.length-1];
-        for (int i = 0; i < temp.length; i++){
-            if (i < posizione) {
-                temp[i] = voti[i];
-            } else if(i >= posizione) {
-                temp[i] = voti[i+1];
-            }
+        if (posizione >= voti.size() || posizione < 0) {
+            throw new Exception("La posizione supera i limiti dell'array");
         }
-        voti = temp;
+        voti.remove(posizione.intValue());
+    }
+
+    public void rimuoviVoto(Float voto) throws Exception {
+        if(voti == null){ 
+            throw new Exception("Non può essere null");
+        }
+        if(voto == null){ 
+            throw new Exception("Non può essere null");
+        }
+        if (voto.floatValue() < 3 || voto.floatValue() > 10) {
+            throw new Exception("Non esistono voti con tale valore");
+        } 
+        voti.remove(voto);
+    }
+
+    public Float mediaVoti() throws Exception{
+        if(voti == null){
+            throw new Exception("Non può essere null");
+        }
+        if(voti.size() == 0){
+            throw new Exception("La media di 0 voti non può essere decisa");
+        }
+
+        Float media = 0.0f;
+
+        for(int i = 0; i < voti.size(); i++){
+            media += voti.get(i).floatValue();
+        }
+
+        return media / voti.size();
+    }
+
+    private ArrayList<Float> ordinamento(TipoOrdinamento t) throws Exception {
+        ArrayList<Float> temp = new ArrayList<Float>();
+        if(voti instanceof ArrayList<?>){
+            temp = (ArrayList<Float>) voti.clone();
+        }
+        if(t == TipoOrdinamento.CRESCENTE){
+            temp.sort(Comparator.naturalOrder());
+        } else if(t == TipoOrdinamento.DECRESCENTE){
+            temp.sort(Comparator.reverseOrder());
+        } else {
+            throw new Exception("Invalid TipoOrdinamento");
+        }
+        return temp;
+    }
+
+    public Float votoMinore() throws Exception {
+        return ordinamento(TipoOrdinamento.CRESCENTE).getFirst().floatValue();
+    }
+
+    public Float votoMaggiore() throws Exception {
+        return ordinamento(TipoOrdinamento.DECRESCENTE).getFirst().floatValue();
+    }
+
+    public void ordinaVotoCrescente() throws Exception {
+        voti = ordinamento(TipoOrdinamento.CRESCENTE); 
+    }
+
+    public void ordinaVotoDecrescente() throws Exception {
+        voti = ordinamento(TipoOrdinamento.DECRESCENTE); 
     }
 
     public Boolean promuovi() throws Exception {
@@ -167,14 +235,14 @@ public class Studente extends Persona {
         }
     }
 
-    private String stampaVoti() {
+    public String stampaVoti() {
         String t = "";
-        if (voti.length == 0) {
+        if (voti.size() == 0) {
             t = "non ci sono voti";
         } else {
-            t = "[" + voti[0];
-            for (int i = 1; i < voti.length; i++) {
-                t += "," + voti[i];
+            t = "Voti: [" + voti.getFirst();
+            for (int i = 1; i < voti.size(); i++) {
+                t += ", " + voti.get(i);
             }
             t += "]";
         }
